0d8a7bd2df580e5ed9529f8b36ec93648ec1b133
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2020-08-23 21:56:51 +0200

    hotfix

--- ttt_ttt2_milk_gun/lua/autorun/shared.lua
+++ ttt_ttt2_milk_gun/lua/autorun/shared.lua
@@ -1,4 +1,7 @@
-// Use string or string.format("%.f",<steamid64>)
-hook.Add( "TTT2FinishedLoading" , "TTT2RegistermexikoediAddonDev" , function( )
-    AddTTT2AddonDev( "76561198279816989" )
-end )
+if CLIENT then
+    // Use string or string.format("%.f",<steamid64>) 
+    // addon dev emblem in scoreboard
+    hook.Add( "TTT2FinishedLoading" , "TTT2RegistermexikoediAddonDev" , function( )
+        AddTTT2AddonDev( "76561198279816989" )
+    end )
+end
