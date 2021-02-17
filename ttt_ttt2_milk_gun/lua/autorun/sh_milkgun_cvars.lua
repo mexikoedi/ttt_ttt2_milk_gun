a83de44f1c158cb637cb0c48cddab4f38d689143
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2021-02-17 21:59:32 +0100

    formatted code

--- ttt_ttt2_milk_gun/lua/autorun/sh_milkgun_cvars.lua
+++ ttt_ttt2_milk_gun/lua/autorun/sh_milkgun_cvars.lua
@@ -1,37 +1,37 @@
-if engine.ActiveGamemode( ) != "terrortown" then return end
+if engine.ActiveGamemode() ~= "terrortown" then return end
 
 if SERVER then
-    AddCSLuaFile( )
+    AddCSLuaFile()
 
-    if file.Exists( "scripts/sh_convarutil.lua" , "LUA" ) then
-        AddCSLuaFile( "scripts/sh_convarutil.lua" )
-        print( "[INFO][Milk Gun] Using the utility plugin to handle convars instead of the local version" )
+    if file.Exists("scripts/sh_convarutil.lua", "LUA") then
+        AddCSLuaFile("scripts/sh_convarutil.lua")
+        print("[INFO][Milk Gun] Using the utility plugin to handle convars instead of the local version")
     else
-        AddCSLuaFile( "scripts/sh_convarutil_local.lua" )
-        print( "[INFO][Milk Gun] Using the local version to handle convars instead of the utility plugin" )
+        AddCSLuaFile("scripts/sh_convarutil_local.lua")
+        print("[INFO][Milk Gun] Using the local version to handle convars instead of the utility plugin")
     end
 end
 
-if file.Exists( "scripts/sh_convarutil.lua" , "LUA" ) then
-    include( "scripts/sh_convarutil.lua" )
+if file.Exists("scripts/sh_convarutil.lua", "LUA") then
+    include("scripts/sh_convarutil.lua")
 else
-    include( "scripts/sh_convarutil_local.lua" )
+    include("scripts/sh_convarutil_local.lua")
 end
 
-// Must run before hook.Add
-local cg = ConvarGroup( "Milkgun" , "Milk Gun" )
+-- Must run before hook.Add
+local cg = ConvarGroup("Milkgun", "Milk Gun")
 
-Convar( cg , false , "ttt_milkgun_automaticFire" , 0 , { FCVAR_ARCHIVE , FCVAR_NOTIFY , FCVAR_REPLICATED } , "Enable automatic fire" , "bool" )
+Convar(cg, false, "ttt_milkgun_automaticFire", 0, {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Enable automatic fire", "bool")
 
-Convar( cg , false , "ttt_milkgun_damage" , 150 , { FCVAR_ARCHIVE , FCVAR_NOTIFY , FCVAR_REPLICATED } , "Damage dealt on impact" , "int" , 1 , 500 )
+Convar(cg, false, "ttt_milkgun_damage", 150, {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Damage dealt on impact", "int", 1, 500)
 
-Convar( cg , false , "ttt_milkgun_randomDamage" , 5 , { FCVAR_ARCHIVE , FCVAR_NOTIFY , FCVAR_REPLICATED } , "Applied on top of the normal damage (+/-)" , "int" , 1 , 200 )
+Convar(cg, false, "ttt_milkgun_randomDamage", 5, {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Applied on top of the normal damage (+/-)", "int", 1, 200)
 
-Convar( cg , false , "ttt_milkgun_ammo" , 3 , { FCVAR_ARCHIVE , FCVAR_NOTIFY , FCVAR_REPLICATED } , "Default ammo the milkgun has when bought" , "int" , 1 , 255 )
+Convar(cg, false, "ttt_milkgun_ammo", 3, {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Default ammo the milkgun has when bought", "int", 1, 255)
 
-Convar( cg , false , "ttt_milkgun_clipSize" , 3 , { FCVAR_ARCHIVE , FCVAR_NOTIFY , FCVAR_REPLICATED } , "Clipsize of the milkgun" , "int" , 1 , 255 )
+Convar(cg, false, "ttt_milkgun_clipSize", 3, {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Clipsize of the milkgun", "int", 1, 255)
 
-Convar( cg , false , "ttt_milkgun_rps" , 0.4 , { FCVAR_ARCHIVE , FCVAR_NOTIFY , FCVAR_REPLICATED } , "Packages of milk to shoot per second" , "float" , 0.01 , 10 , 2 )
-//
-//generateCVTable()
-//
+Convar(cg, false, "ttt_milkgun_rps", 0.4, {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Packages of milk to shoot per second", "float", 0.01, 10, 2)
+--
+-- generateCVTable()
+--
