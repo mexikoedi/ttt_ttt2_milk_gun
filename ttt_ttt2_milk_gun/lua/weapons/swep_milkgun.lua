198f77c08bc96d9fa11981ad097ae4fdaaa1b095
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2021-04-12 16:25:31 +0200

    small optimization

--- ttt_ttt2_milk_gun/lua/weapons/swep_milkgun.lua
+++ ttt_ttt2_milk_gun/lua/weapons/swep_milkgun.lua
@@ -43,68 +43,76 @@ local ShootSound = Sound("milk.wav")
 local SecondSound = Sound("milk_altfire.wav")
 
 function SWEP:Initialize()
-    if (CLIENT) then return end
+    if CLIENT then return end
     self.Primary.ClipSize = GetConVar("ttt_milkgun_clipSize"):GetInt()
     self.Primary.DefaultClip = GetConVar("ttt_milkgun_ammo"):GetInt()
     self.Primary.Automatic = GetConVar("ttt_milkgun_automaticFire"):GetBool()
     self.Primary.RPS = GetConVar("ttt_milkgun_rps"):GetFloat()
 end
 
-function SWEP:PrimaryAttack()
-    self:SetNextPrimaryFire(CurTime() + 1 / self.Primary.RPS)
-    if not self:CanPrimaryAttack() then return end
-    self:TakePrimaryAmmo(1)
-    self:EmitSound(ShootSound)
-    if (CLIENT) then return end
-    local ent = ents.Create("sent_milkgun")
-    if (not IsValid(ent)) then return end
-    ent:SetModel("models/props_junk/garbage_milkcarton002a.mdl")
-    ent:SetAngles(self:GetOwner():EyeAngles())
-    ent:SetPos(self:GetOwner():EyePos() + (self:GetOwner():GetAimVector() * 16))
-    ent:SetOwner(self:GetOwner()) -- Prevents all normal phys damage to all entities for whatever reason, but we actually want this to be the case
-    ent:Spawn()
-    ent.Owner = self:GetOwner()
-    ent:Activate()
-    util.SpriteTrail(ent, 0, Color(255, 255, 255), false, 16, 1, 6, 1 / (15 + 1) * 0.5, "trails/laser.vmt")
-    local phys = ent:GetPhysicsObject()
-
-    if (not IsValid(phys)) then
-        ent:Remove()
-
-        return
-    end
-
-    phys:SetMass(2)
-    phys:SetVelocity(self:GetOwner():GetAimVector() * 100000)
-    local anglo = Angle(-10, -5, 0)
-    self:GetOwner():ViewPunch(anglo)
-end
-
-function SWEP:SecondaryAttack()
-    self:SetNextSecondaryFire(CurTime() + 5)
-
-    if SERVER then
+if SERVER then
+    function SWEP:PrimaryAttack()
         self.currentOwner = self:GetOwner()
-        self:GetOwner():EmitSound(SecondSound)
+        self:SetNextPrimaryFire(CurTime() + 1 / self.Primary.RPS)
+        if not self:CanPrimaryAttack() then return end
+        self:TakePrimaryAmmo(1)
+
+        if GetConVar("ttt_milkgun_primary_sound"):GetBool() then
+            self.currentOwner:EmitSound(ShootSound)
+        end
+
+        local ent = ents.Create("sent_milkgun")
+        if (not IsValid(ent)) then return end
+        ent:SetModel("models/props_junk/garbage_milkcarton002a.mdl")
+        ent:SetAngles(self.currentOwner:EyeAngles())
+        ent:SetPos(self.currentOwner:EyePos() + (self.currentOwner:GetAimVector() * 16))
+        ent.Owner = self.currentOwner
+        ent:SetOwner(self.currentOwner) -- Prevents all normal phys damage to all entities for whatever reason, but we actually want this to be the case
+        ent:SetPhysicsAttacker(self.currentOwner)
+        ent.fingerprints = self.fingerprints
+        ent:Spawn()
+        ent:Activate()
+        util.SpriteTrail(ent, 0, Color(255, 255, 255), false, 16, 1, 6, 1 / (15 + 1) * 0.5, "trails/laser.vmt")
+        local phys = ent:GetPhysicsObject()
+
+        if (not IsValid(phys)) then
+            ent:Remove()
+
+            return
+        end
+
+        phys:SetMass(2)
+        phys:SetVelocity(self.currentOwner:GetAimVector() * 100000)
+        local anglo = Angle(-10, -5, 0)
+        self.currentOwner:ViewPunch(anglo)
     end
-end
 
-function SWEP:Holster()
-    if SERVER and IsValid(self.currentOwner) then
-        self.currentOwner:StopSound("milk_altfire.wav")
+    function SWEP:SecondaryAttack()
+        self.currentOwner = self:GetOwner()
+        self:SetNextSecondaryFire(CurTime() + 5)
+
+        if GetConVar("ttt_milkgun_secondary_sound"):GetBool() then
+            self.currentOwner:EmitSound(SecondSound)
+        end
     end
 
-    return true
-end
+    function SWEP:Holster()
+        if IsValid(self.currentOwner) then
+            self.currentOwner:StopSound("milk_altfire.wav")
+        end
 
-function SWEP:OnRemove()
-    if SERVER and IsValid(self.currentOwner) then
-        self.currentOwner:StopSound("milk_altfire.wav")
+        return true
     end
-end
 
-function SWEP:OnDrop()
-    if SERVER and IsValid(self.currentOwner) then
-        self.currentOwner:StopSound("milk_altfire.wav")
+    function SWEP:OnRemove()
+        if IsValid(self.currentOwner) then
+            self.currentOwner:StopSound("milk_altfire.wav")
+        end
+    end
+
+    function SWEP:OnDrop()
+        if IsValid(self.currentOwner) then
+            self.currentOwner:StopSound("milk_altfire.wav")
+        end
     end
 end
